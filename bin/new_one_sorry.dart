1ed2ce1ba066416630646fd5a912c51f08b8507e
muslima <muslima241105@gmail.com>
2023-11-22 16:49:18 +0500

    hello

--- bin/new_one_sorry.dart
+++ bin/new_one_sorry.dart
@@ -1,5 +1,6 @@
 import 'package:new_one_sorry/new_one_sorry.dart' as new_one_sorry;
 
 void main(List<String> arguments) {
-  print('Hello world: ${new_one_sorry.calculate()}!');
+  String hello = 'Hello';
+  print('Hello world: $hello');
 }
